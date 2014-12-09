85b7005d605c508e595f8f8c71b4d1022958c36d
tharrisx1 <tharris.email@gmail.com>
2014-12-08 18:40:20 -0800

    Modifying the load test parameters

--- src/test/java/org/tharrisx/test/framework/rest/suites/UserLoadSuite.java
+++ src/test/java/org/tharrisx/test/framework/rest/suites/UserLoadSuite.java
@@ -6,11 +6,11 @@ import org.tharrisx.test.framework.rest.stories.UserStory;
 
 public class UserLoadSuite {
 
-  private static final int USERS = 50;
-  private static final int ITERATIONS = 100;
-  private static final long CONSTANT_DELAY = 50;
+  private static final int USERS = 20;
+  private static final int ITERATIONS = 500;
+  private static final long CONSTANT_DELAY = 1000;
   private static final double DELAY_VARIATION = 0.2;
-  private static final long MAX_ELAPSED_TIME = 60000;
+  private static final long MAX_ELAPSED_TIME = 30000;
 
   public static Test suite() {
     return LoadGenerator.createLoadTest(UserStory.class, USERS, ITERATIONS, CONSTANT_DELAY, DELAY_VARIATION, MAX_ELAPSED_TIME);
